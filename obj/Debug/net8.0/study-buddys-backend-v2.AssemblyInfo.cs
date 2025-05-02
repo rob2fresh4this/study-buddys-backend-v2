57ddf3f8da46d872a03d0ad63f96b72a24d7a9fc
rob2fresh4this <grijrob07@gmail.com>
2025-05-02 11:05:56 -0700

    Add resource DLLs for multiple languages and update project files
    - Added new resource DLLs for Turkish (tr), Simplified Chinese (zh-Hans), and Traditional Chinese (zh-Hant) for Microsoft.CodeAnalysis and related assemblies.
    - Updated AssemblyInfo.cs with a new informational version.
    - Introduced new static web assets build files and properties for the project.
    - Generated new binaries and updated cache files for the project.
    - Added sourcelink support for debugging.

--- obj/Debug/net8.0/study-buddys-backend-v2.AssemblyInfo.cs
+++ obj/Debug/net8.0/study-buddys-backend-v2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("study-buddys-backend-v2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c99e9e70efb13da8cd9af381da0134272218fdba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6ec90497fdbe14ac289a3705bbd2f8eefedba4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("study-buddys-backend-v2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("study-buddys-backend-v2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
